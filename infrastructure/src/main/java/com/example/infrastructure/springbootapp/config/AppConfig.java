1e97dbe80e9a89db75547e62c1b36a1608216401
Rafal Kondratowicz <rkondr@gmail.com>
2022-04-19 11:53:23 +0200

    create project

---
+++ infrastructure/src/main/java/com/example/infrastructure/springbootapp/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.example.infrastructure.springbootapp.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class AppConfig {
+    // Add config here
+}
